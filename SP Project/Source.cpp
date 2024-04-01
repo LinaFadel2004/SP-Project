540b137543a4f8897e313b135903bb28d6ed157a
LinaFadel2004 <165325869+LinaFadel2004@users.noreply.github.com>
2024-04-01 13:37:01 +0200

    Update Source.cpp

--- SP Project/Source.cpp
+++ SP Project/Source.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string >
+#include <conio.h>
+#include <ctime>
 using namespace std;
+string Path = "data\info.text";
 int main() {
 	
-	cout << "Online Exam System";
-
+	cout << "\tOnline Exam System\t\n";
+        ofstream file (Path, ios::app | ios::out | ios::ate);
 	return 0;
 }
