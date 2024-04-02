776196fcfd5b09d124961f6e57debd41c2e606e3
LinaFadel2004 <165325869+LinaFadel2004@users.noreply.github.com>
2024-04-02 02:03:07 +0200

    Update Source.cpp

--- SP Project/Source.cpp
+++ SP Project/Source.cpp
@@ -1,30 +1,53 @@
 #include <iostream>
+#include <string>
 #include <fstream>
-#include <string >
 #include <conio.h>
 #include <ctime>
+
 using namespace std;
 
-#define Path "data\\info.text"
+#define info_path "data/info.txt"
+
+void sign_up(string, string);
 
 int main() {
-	string username;
-	char correctpassword[];
-	
-	cout << "\tOnline Exam System\t\n";
-	ofstream file;
-        file.open (Path, ios::app | ios::out | ios::ate);
+    string username;
+    string password = "";
+    cout << "\t\t Online Exam System \t\t\n";
+    cout << "\t\t ------------------ \t\t\n";
+    sign_up(username, password);
 
-	cout << "username:\t\t";
-	getline(cin, username);
-	file << username << endl;
-	cout << "password:\t\t";
-	correctpassword=cin.get();
-	while (correctpassword!="\n"){
-		cout << "*";
-	correctpassword=cin.get();
-	file << correctpassword << endl;
-	}
-	file.close();
-	return 0;
 }
+
+void sign_up(string username, string password) {
+    cout << "username: ";
+    getline(cin, username);
+    cout << "password: ";
+    char ch;
+    while ((ch = _getch()) != '\r')
+    {
+        if (ch == '\b')
+        {
+            if (!password.empty()) {
+                cout << "\b \b"; // Move cursor back and erase character
+                password.pop_back(); // Remove last character from password
+                cout.flush(); // Flush the output buffer
+            }
+        }
+        else {
+                cout << "*";
+                password += ch;
+            }
+    }
+        ofstream myfile;
+        myfile.open(info_path, ios::out | ios::app); //out:أقدر أكتب فيه ,app:لما افتح الملف للمرة التانية الاقي البيانات محفوظة
+        if (myfile.is_open())
+        {
+            myfile << username << endl;
+            myfile << password << endl;
+            myfile.close();
+        }
+        else
+            cout << "Error: Unable to open file." << endl;
+
+    }
